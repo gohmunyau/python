deed82048e44db6c170a5e108c4b5cc64d89003b
gohmunyau <140488543+gohmunyau@users.noreply.github.com>
2024-01-25 17:24:49 +0800

    Update test_pythonfile.py

--- test_pythonfile.py
+++ test_pythonfile.py
@@ -6,6 +6,17 @@ import socket
 import psutil
 import requests
 import unittest
+from pythonfile import SystemMonitor
+
+class TestSystemMonitor(unittest.TestCase):
+    def test_disk_usage(self):
+        # Your test code here
+        pass
+
+    def test_cpu_utilization(self):
+        # Your test code here
+        pass
+
 
 class SystemMonitor:
     """
