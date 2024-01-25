22b2edc9ff515388066b95c9ca66e0643cc38b02
gohmunyau <140488543+gohmunyau@users.noreply.github.com>
2024-01-25 17:32:11 +0800

    Update and rename test to test_pythonfiles.py

---
+++ test_pythonfiles.py
@@ -0,0 +1,11 @@
+import unittest
+from pythonfile import SystemMonitor
+
+class TestSystemMonitor(unittest.TestCase):
+    def test_disk_usage(self):
+        # Your test code here
+        pass
+
+    def test_cpu_utilization(self):
+        # Your test code here
+        pass
